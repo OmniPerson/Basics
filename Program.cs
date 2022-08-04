58ec3f10dd818b6e4704bb6b3bb620d829726a57
OmniPerson <ginthetonic@gmail.com>
2022-08-04 19:01:10 +0300

    bruh

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿List<string> groceries = new List<string>(){ "onions", "cucumber", "harbuz", "apples"};
+
+var items = String.Join("," , groceries);
+//Homework
+//make 3 lists all containing diffrent items, from whisc tasks, groceries and whatever
+//all user imput to determine which list gets selected 
+// if-else or switch  statements
+
+
+

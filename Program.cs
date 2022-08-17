eb1da500826ce7359ecef388d99098b3098bd85f
OmniPerson <ginthetonic@gmail.com>
2022-08-17 19:03:54 +0300

    remade the list

--- Program.cs
+++ Program.cs
@@ -1,62 +1,59 @@
 ﻿
-//Homework
-//make 3 lists all containing diffrent items, from whisc tasks, groceries and whatever {check}
-//allow user imput to determine which list gets selected 
-// if-else or switch  statements
-//bonus: include join function
 
-//HW-07-08-22
 
 List<string> tasks = new List<string>() { "publish", "rectify", "endorse" };
 List<string> groceries = new List<string>() { "apple", "flour", "eggs" };
 List<string> whatevers = new List<string>() { "pen", "book", "crayons" };
-//var taskarray = String.Join(",", tasks);
-//var groceriesarray = String.Join(",", groceries);
-//var whateversarray = String.Join(",", whatevers);
-Menu menu = new Menu("Main Menu");
-MenuItem item1 = new MenuItem("Tasks");
-MenuItem item2 = new MenuItem("Groceries");
-MenuItem item3 = new MenuItem("Whatevers");
-menu.Items.Add(item1);
-menu.Items.Add(item2);
-menu.Items.Add(item3);
 
 
-Menu menu2 = new Menu("Which list do you wish to display?", new List<MenuItem>(){
-  item1,
-  item2,
-  item3
+Menu menu = new Menu("Which list do you wish to display?", new List<MenuItem>(){
+  new MenuItem("Tasks"),
+  new MenuItem("Groceries"),
+  new MenuItem("Whatevers")
 });
-
 
 
 while (true)
 {
-    for(int i = 0; i <= 5; i++){
-        Console.WriteLine("-");
+
+    for (int i = 0; i <= 20; i++)
+    {
+        Console.Write("-");
     }
-    Console.WriteLine(menu2.Title);
-Console.WriteLine($"Type 'tasks' to display the items contained in the -{item1.Label}- list!");
-Console.WriteLine($"Type 'groceries' to display the items contained in the -{item2.Label}- list!");
-Console.WriteLine($"Type 'whatevers' to display the items contained in the -{item3.Label}- list!");
-//TASK: display each menu item from menu 2
-//TASK2 : get rid of the joins , print each item on a new line
-//Foreach loop -> display items
+    Console.WriteLine();
+    Console.WriteLine(menu.Title);
+    foreach (MenuItem item in menu.Items)
+    {
+        Console.WriteLine($"Type '{item.Label.ToLower()}' to display the items contained in the -{item.Label}- list!");
+    }
+
     string? choice = Console.ReadLine();
 
     switch (choice)
     {
 
         case "tasks":
-            Console.WriteLine($"The list contains the following items: {item1.Label} ;");
+            Console.WriteLine("The list contains the following items:  ");
+            foreach (var task in tasks)
+            {
+                Console.WriteLine(task);
+            }
             break;
 
         case "groceries":
-            Console.WriteLine($"The list contains the following items: {item2.Label} ;");
+            //Console.WriteLine($"The list contains the following items: {item2.Label} ;");
+            foreach (var grocery in groceries)
+            {
+                Console.WriteLine(grocery);
+            }
             break;
 
         case "whatevers":
-            Console.WriteLine($"The list contains the following items: {item3.Label} ;");
+            //Console.WriteLine($"The list contains the following items: {item3.Label} ;");
+            foreach (var whatever in whatevers)
+            {
+                Console.WriteLine(whatever);
+            }
             break;
 
         default:
@@ -69,9 +66,3 @@ Console.WriteLine($"Type 'whatevers' to display the items contained in the -{ite
 }
 
 
-foreach (object o in tasks)
-{
-Console.WriteLine(o);
-
-
-}
